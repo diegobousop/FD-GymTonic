491bce6e5ea4e62219f540b87136b0afbbd790ac
Aarón Eiroa López <aaron.eiroa@udc.es>
2025-11-16 13:08:27 +0100

    [BACKEND] Añadido el routineId al DTO para poder navegar a las rutinas. refs #4291 @.5h

--- src/main/java/es/udc/fi/dc/fd/rest/dtos/NotificationConversor.java
+++ src/main/java/es/udc/fi/dc/fd/rest/dtos/NotificationConversor.java
@@ -1,16 +1,15 @@
 package es.udc.fi.dc.fd.rest.dtos;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import es.udc.fi.dc.fd.model.entities.Notification;
 
 public class NotificationConversor {
     public static NotificationDto toNotificationDto(Notification notification) {
-        return new NotificationDto(notification.getId(),notification.getMessage(),notification.getIsRead(),notification.getDate().toString());
+        return new NotificationDto(notification.getId(), notification.getRoutine().getId(), notification.getMessage(), notification.getIsRead(), notification.getDate().toString());
     }
 
     public static List<NotificationDto> toNotificationDtos(List<Notification> notifications) {
-        return notifications.stream().map(c -> toNotificationDto(c)).collect(Collectors.toList());
+        return notifications.stream().map(c -> toNotificationDto(c)).toList();
     }
 }
